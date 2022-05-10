a6bb0a26dc032794d7c55e65c522eb3a3fa65f12
Nguyen Van Quyen <quyennv.uet@gmail.com>
2022-05-10 16:24:12 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5f295479eb6df8f65533f10e20a7682f",
-"index.html": "6771207430d244047419248162b2ab4f",
-"/": "6771207430d244047419248162b2ab4f",
+"index.html": "2fe39864d524a8a269899fb25270717a",
+"/": "2fe39864d524a8a269899fb25270717a",
 "firebase-messaging-sw.js": "00c6d90db396febd110f41a847633fa1",
-"main.dart.js": "91d8a5d750adeee2c505cf5bee0e1e98",
+"main.dart.js": "17dd76bafc91deaee856e3410e3831ff",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
